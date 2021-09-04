3b42937fd0b940964dab8dd30390add3fe2dc8d9
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-04 14:44:30 +0300

    Created an if statement for encrypt option

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner option = new Scanner(System.in);
-        System.out.println("Select 1 :to Encrypt or 2 :To decrypt , and 3 to Exit fro program");
+        System.out.println("Select 1:'Encrypt' or 2:'decrypt' or  3:'Exit' for action program");
 
         Integer Select = option.nextInt();
+
+        if(Select ==1){
+            Scanner Text = new Scanner(System.in);
+            System.out.println("Enter Text to Encrypt");
+            String text = Text.nextLine();
+            System.out.println("you inputted" + text);
+        }
     }
 }
