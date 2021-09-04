d4a71d6dbfd8523132727d9215d146bfa752e109
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-04 19:55:51 +0300

    Passed test for the decrypt

--- src/test/java/CipherDecryptTest.java
+++ src/test/java/CipherDecryptTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CipherDecryptTest {
-
+    @Test
+    public void decriptTest(){
+        CipherDecrypt cipherdecrypt = new CipherDecrypt(23,"hey");
+        assertEquals(true, cipherdecrypt instanceof CipherDecrypt);
+    }
 }
