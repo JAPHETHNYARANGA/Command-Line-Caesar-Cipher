b7ccf1e50d5453101a08f11f1b3716671556c93d
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-04 13:45:14 +0300

    created encryption method with a for loop and modifications to encrypt the data

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -12,4 +12,19 @@ public class Encrypt {
     public int getKey(){
         return key;
     }
+
+    public String encryption(){
+        StringBuilder encryption = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(this.text.charAt(i))) {
+                char e = (char) (((int) this.text.charAt(i) + this.key - 50) % 20 + 55);
+                encryption.append(e);
+            } else {
+                char e = (char) (((int) this.text.charAt(1) + this.key - 45) % 30 + 45);
+                encryption.append(e);
+            }
+        }
+        return encryption.toString();
+    }
+    
 }
