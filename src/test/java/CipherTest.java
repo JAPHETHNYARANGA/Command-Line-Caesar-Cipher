1d38aaf47c262c4944b984c05201bd8c72b414f8
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-04 20:38:00 +0300

    re introduced all the test cases

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -3,5 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CipherTest {
+@Test
+public void testCipher(){
 
+    public void encrypt(){
+        CipherDecrypt cipherdecrypt = new CipherDecrypt(23,"hey");
+        assertEquals(true, cipherdecrypt instanceof CipherDecrypt);
+    }
+}
 }
