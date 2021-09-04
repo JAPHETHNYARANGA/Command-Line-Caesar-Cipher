c92d595f61e3f463c48ff6e97031334594419647
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-04 12:59:16 +0300

    created a getInput method and a get key method to encrypt the  data

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,15 @@
 public class Encrypt {
+    private int key;
+    private String text;
+
+    public Encrypt( int key, String text){
+        this.key = key;
+        this.text = text;
+    }
+    public String getInput(){
+        return text;
+    }
+    public int getKey(){
+        return key;
+    }
 }
