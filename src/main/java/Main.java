0a811395db078f6f33d042f39eff319463f68a99
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-04 12:39:24 +0300

    Created The output and the select to choose from

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner option = new Scanner(System.in);
+        System.out.println("Select 1 :to Encrypt or 2 :To decrypt , and 3 to Exit fro program");
+
+        Integer Select = option.nextInt();
     }
 }
